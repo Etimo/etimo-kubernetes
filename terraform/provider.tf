6478a28a6ef7fae9535eb0fbd40bfd5d457ba281
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2022-03-15 20:38:48 +0100

    terraform: add initial provider

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+variable "do_token" {}
+variable "pvt_key" {}
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
