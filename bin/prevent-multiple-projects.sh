0b61653f69bb46c0c7d1e7637305cc04bdbbe55b
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2022-03-16 15:03:27 +0100

    test

--- bin/prevent-multiple-projects.sh
+++ bin/prevent-multiple-projects.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-git diff --name-only origin/main HEAD
+PROJECT_COUNT=$(git diff --name-only origin/main HEAD | awk -F'/' '{print $2}' | sort | uniq | wc -l)
+if [ $PROJECT_COUNT -gt 1 ]; then
+    echo "Multiple projects detected"
+    exit 1
+fi
