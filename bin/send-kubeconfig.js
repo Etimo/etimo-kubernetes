e54cf679b2a4297f08e9dafa8976e0d60d824c44
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2022-03-21 08:40:30 +0100

    fix: add dry-run flag to email sending script

--- bin/send-kubeconfig.js
+++ bin/send-kubeconfig.js
@@ -1,5 +1,6 @@
 const { program } = require("commander");
 const consts = require("../lib/consts");
+const fs = require("fs");
 const nodemailer = require("nodemailer");
 const handlebars = require("handlebars");
 const { getTemplate } = require("../lib/templates");
@@ -7,6 +8,7 @@ const { schemaGithubUsername, assertValidData } = require("../lib/schemas");
 const { assertFile } = require("../lib/file");
 const { getKubeconfigFileForUsername } = require("../lib/consts");
 const { readClusterInfo } = require("../lib/cluster-info");
+const { getRecipientFromGithubUsername } = require("../lib/email");
 
 // Cmd
 const options = program
@@ -26,30 +28,45 @@ assertValidData(username, schemaGithubUsername);
 assertFile(consts.FILENAME_CLUSTER_INFO, true);
 
 const clusterInfo = readClusterInfo();
-const attachments = clusterInfo.map((cluster) => {
-  const stage = cluster.stage;
-  const kubeconfigFile = getKubeconfigFileForUsername(username, stage);
-});
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  auth: {
-    user: mailUsername,
-    pass: mailPassword,
-  },
-});
+const attachments = clusterInfo
+  .map((cluster) => {
+    const stage = cluster.stage;
+    const kubeconfigFile = getKubeconfigFileForUsername(username, stage);
+    return fs.existsSync(kubeconfigFile) ? kubeconfigFile : null;
+  })
+  .filter(Boolean)
+  .map((path) => ({ path }));
+console.log("Found the following kubeconfigs:", attachments);
+
+console.log("Rendering email...");
 const context = {
   username,
+  attachments,
 };
+const subject = getTemplate(handlebars, "email", "subject.hbs")(context);
+const html = getTemplate(handlebars, "email", "body.hbs")(context);
+const to = getRecipientFromGithubUsername(username);
+
 const message = {
   from: "daniel.winther@etimo.se",
-  to: "daniel.winther@etimo.se",
-  subject: getTemplate(handlebars, "email", "subject.hbs")(context),
-  html: getTemplate(handlebars, "email", "body.hbs")(context),
-  attachments: [
-    {
-      path: kubeconfigFile,
-    },
-  ],
+  to,
+  subject,
+  html,
+  attachments,
 };
-transporter.sendMail(message).then((res) => console.log(res));
+
+console.log("Sending email...");
+if (!dryRun) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    auth: {
+      user: mailUsername,
+      pass: mailPassword,
+    },
+  });
+  transporter.sendMail(message).then((res) => console.log(res));
+  console.log("  -> Email sent!");
+} else {
+  console.log("  -> Skipping email sending because of dry run");
+}
