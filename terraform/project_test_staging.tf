95b6390fb805a03e3a424cb5ee9f2e3f10ac074d
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2022-03-27 22:04:41 +0200

    fix: formatting of terraform in workflow

--- terraform/project_test_staging.tf
+++ terraform/project_test_staging.tf
@@ -2,7 +2,7 @@
 
 locals {
   test_staging_database_clusters = toset([])
-  test_staging_shared_databases = toset([])
+  test_staging_shared_databases  = toset([])
 }
 
 resource "digitalocean_database_db" "test_staging_shared" {
@@ -31,32 +31,32 @@ resource "digitalocean_database_cluster" "test_staging" {
 output "project__test_staging" {
   value = {
     project = "test"
-    stage = "staging"
+    stage   = "staging"
     database_clusters = {
-      for key, obj in digitalocean_database_cluster.test_staging:
+      for key, obj in digitalocean_database_cluster.test_staging :
       key => ({
-        name: obj.name,
-        user: obj.user,
-        password: obj.password,
-        port: obj.port
-        host: obj.host
-        private_host: obj.private_host
+        name : obj.name,
+        user : obj.user,
+        password : obj.password,
+        port : obj.port
+        host : obj.host
+        private_host : obj.private_host
       })
     }
     shared_databases = {
-      for key, obj in digitalocean_database_db.test_staging_shared:
+      for key, obj in digitalocean_database_db.test_staging_shared :
       key => ({
-        name: obj.name,
-        port: digitalocean_database_cluster.staging-shared.port
-        host: digitalocean_database_cluster.staging-shared.host
-        private_host: digitalocean_database_cluster.staging-shared.private_host
+        name : obj.name,
+        port : digitalocean_database_cluster.staging-shared.port
+        host : digitalocean_database_cluster.staging-shared.host
+        private_host : digitalocean_database_cluster.staging-shared.private_host
       })
     }
     shared_databases_users = {
-      for key, obj in digitalocean_database_user.test_staging_shared:
+      for key, obj in digitalocean_database_user.test_staging_shared :
       key => ({
-        name: obj.name,
-        password: obj.password,
+        name : obj.name,
+        password : obj.password,
       })
     }
   }
