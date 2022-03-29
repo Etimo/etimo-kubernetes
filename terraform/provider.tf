cac8f0097c10178c2516e6cf81829f1d65977a4b
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2022-03-29 22:52:55 +0200

    feat: add logdna view for each project

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,12 @@
-variable "do_token" {}
-variable "spaces_key" {}
-variable "spaces_secret" {}
-
 terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
+    }
+    logdna = {
+      source  = "logdna/logdna"
+      version = "1.7.0"
     }
   }
 
@@ -23,4 +23,8 @@ provider "digitalocean" {
   token             = var.do_token
   spaces_access_id  = var.spaces_key
   spaces_secret_key = var.spaces_secret
+}
+
+provider "logdna" {
+  servicekey = var.logdna_key
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,8 @@ variable "stages" {
   type    = list(string)
   default = ["Staging"] #, "Production"]
 }
+
+variable "do_token" {}
+variable "spaces_key" {}
+variable "spaces_secret" {}
+variable "logdna_key" {}
