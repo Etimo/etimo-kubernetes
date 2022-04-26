c564155a6641c4dbffd1e851d159fa3ea2c26a3f
release <>
2022-04-26 08:59:29 +0000

    release: 2022-04-26T08:59:29Z [skip ci]

--- terraform/project_achievements-test_staging.tf
+++ terraform/project_achievements-test_staging.tf
@@ -2,7 +2,7 @@
 
 locals {
   achievements_test_staging_database_clusters = toset([])
-  achievements_test_staging_shared_databases  = toset(["main"])
+  achievements_test_staging_shared_databases  = toset(["main", "unleash"])
 }
 
 resource "digitalocean_database_db" "achievements_test_staging_shared" {
