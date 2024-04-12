1c1f2a58eb757650a39a029c0a29146687ceb96e
release <>
2024-04-12 13:39:31 +0000

    release: 2024-04-12T13:39:31Z [skip ci]

--- terraform/project_activity-tracker_staging.tf
+++ terraform/project_activity-tracker_staging.tf
@@ -1,6 +1,15 @@
 # Buckets, if any
 
 
+resource "digitalocean_database_db" "activity_tracker_staging_main_shared" {
+  cluster_id = local.staging_db_clusters["mysql"].id
+  name       = "activity_tracker_staging_main"
+}
+
+resource "digitalocean_database_user" "activity_tracker_staging_main_shared" {
+  cluster_id = local.staging_db_clusters["mysql"].id
+  name       = "activity_tracker_staging_main"
+}
 
 # Outputs
 output "project__activity_tracker_staging" {
@@ -8,8 +17,20 @@ output "project__activity_tracker_staging" {
     project = "activity-tracker"
     stage   = "staging"
     shared_databases = {
+      main = {
+        db_type      = "mysql",
+        name         = "activity_tracker_staging_main",
+        port         = local.staging_db_clusters["mysql"].port,
+        host         = local.staging_db_clusters["mysql"].host,
+        private_host = local.staging_db_clusters["mysql"].private_host,
+        ca           = local.staging_db_certs["mysql"].certificate,
+      },
     },
     shared_databases_users = {
+      main = {
+        name     = "activity_tracker_staging_main",
+        password = digitalocean_database_user.activity_tracker_staging_main_shared.password,
+      },
     },
   }
   sensitive = true
